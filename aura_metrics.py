0d2cd700b56e9d4969f0922b157ff429a3e45ea2
Oni-giri <89288375+Oni-giri@users.noreply.github.com>
2022-09-14 17:04:49 +0300

    Update aura_metrics.py

--- aura_metrics.py
+++ aura_metrics.py
@@ -33,7 +33,6 @@ def pretty(number, decimals, ether):
 # Coingecko
 
 
-@st.cache
 def cg_data(id, keys):
     data = requests.get(
         f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={id}&order=market_cap_desc&per_page=100&page=1&sparkline=false").content
@@ -44,7 +43,6 @@ def cg_data(id, keys):
     return data_dict
 
 
-@st.cache
 def get_avg_bribe():
     data = requests.post("https://api.llama.airforce/bribes",
                          json={'platform': "hh", 'protocol': "aura-bal"}).json()
